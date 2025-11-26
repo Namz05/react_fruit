cc185ee77ed8d2643ee70cfc7f2a1f8c5afc90c1
Namz05 <namouzou4@gmail.com>
2025-11-26 22:06:47 +0100

    4e ou 5e commit je sais plus

---
+++ src/autres/data.js
@@ -0,0 +1,17 @@
+import home1 from '../assets/images/home1.png'
+import home2 from '../assets/images/home2.png'
+import home3 from '../assets/images/home3.png'
+import home4 from '../assets/images/home4.png'
+import home5 from '../assets/images/home5.png'
+import home6 from '../assets/images/home6.png'
+import home7 from '../assets/images/home7.png'
+
+export const images = {
+    home1,
+    home2,
+    home3,
+    home4,
+    home5,
+    home6,
+    home7,
+};
--- src/components/Accueil/Accueil.jsx
+++ src/components/Accueil/Accueil.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 import "./Accueil.scss"
+import { SiGooglemaps } from "react-icons/si";
 
 const Accueil = () => {
-  return <div>Accueil</div>
+  return <div className= "accueil-grid">
+    <div className="info">
+      <h1>Decouvrez la residence de vos reves</h1>
+      <p>Trouvwz une residence n'a jamais ete aussi facile</p>
+      <div className="conteneur-recherche">
+        <input type="text" />
+        <SiGooglemaps className="icon"/>
+        span
+      </div>
+    </div>
+  </div>
 
 }
 
